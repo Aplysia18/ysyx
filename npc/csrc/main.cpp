d76d841042c1d3713f06703da9c427de12292e94
tracer-ysyx <tracer@ysyx.org>
2024-07-23 22:22:27 +0800

    > sim RTL ysyx_22040000 鹿天瑶 Linux LAPTOP-T76LQ53S 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  22:22:27 up 5 days,  4:14,  0 users,  load average: 0.00, 0.04, 0.06

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,24 @@
+#include "Vtop.h"
+#include "verilated.h"
+#include "verilated_fst_c.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <nvboard.h>
 
-int main() {
-  printf("Hello, ysyx!\n");
+static TOP_NAME dut;
+
+void nvboard_bind_all_pins(TOP_NAME* top);
+
+int main(int argc, char** argv) {
+  nvboard_bind_all_pins(&dut);
+  nvboard_init();
+
+  while(1){
+    nvboard_update();
+  }
+
+  nvboard_quit();
+
   return 0;
 }
