619e824f350be671bb1785d6910746d6f7ad7b78
tracer-ysyx <tracer@ysyx.org>
2024-07-23 22:54:04 +0800

    > sim RTL ysyx_22040000 鹿天瑶 Linux LAPTOP-T76LQ53S 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  22:54:04 up 5 days,  4:45,  0 users,  load average: 0.08, 0.05, 0.07

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -6,15 +6,24 @@
 #include <assert.h>
 #include <nvboard.h>
 
-static TOP_NAME dut;
-
 void nvboard_bind_all_pins(TOP_NAME* top);
 
 int main(int argc, char** argv) {
-  nvboard_bind_all_pins(&dut);
+
+  VerilatedContext* contextp = new VerilatedContext;
+  contextp->commandArgs(argc, argv);
+  Vtop* top = new Vtop{contextp};
+
+  VerilatedFstC* tfp = new VerilatedFstC;
+  Verilated::traceEverOn(true);
+  top->trace(tfp, 99);
+  tfp->open("simx.fst");
+  
+  nvboard_bind_all_pins(top);
   nvboard_init();
 
   while(1){
+    top->eval();
     nvboard_update();
   }
 
