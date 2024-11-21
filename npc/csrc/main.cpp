4374809ea49ad6858763f5f509c86ebb0a00fae7
tracer-ysyx <tracer@ysyx.org>
2024-11-21 10:44:08 +0800

    > sim RTL ysyx_24110015 鹿天瑶 Linux LAPTOP-T76LQ53S 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:44:08 up 20:16,  1 user,  load average: 0.77, 0.55, 0.22

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -17,6 +17,10 @@ static uint8_t pmem[CONFIG_MSIZE] PG_ALIGN = {};
 uint8_t* guest_to_host(paddr_t paddr) { return pmem + paddr - CONFIG_MBASE; }
 paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
 
+static inline bool in_pmem(paddr_t addr) {
+  return addr - CONFIG_MBASE < CONFIG_MSIZE;
+}
+
 static word_t pmem_read(paddr_t addr) {
   word_t ret = *guest_to_host(addr);
   return ret;
@@ -24,6 +28,20 @@ static word_t pmem_read(paddr_t addr) {
 
 static void pmem_write(paddr_t addr, word_t data) {
   *guest_to_host(addr) = data;
+}
+
+word_t paddr_read(paddr_t addr) {
+  if(!in_pmem(addr)) {
+    printf("paddr_read: invalid address 0x%x\n", addr);
+    assert(0);
+  }else return pmem_read(addr);
+}
+
+void paddr_write(paddr_t addr, word_t data) {
+  if(!in_pmem(addr)) {
+    printf("paddr_write: invalid address 0x%x\n", addr);
+    assert(0);
+  }else return pmem_write(addr, data);
 }
 
 int main(int argc, char** argv) {
@@ -38,15 +56,15 @@ int main(int argc, char** argv) {
   tfp->open("./build/simx.vcd");
   
   // 初始化内存
-  pmem_write(0x80000000, 0x00108093);
-  pmem_write(0x80000004, 0x00208093);
-  pmem_write(0x80000008, 0x00308093);
-  pmem_write(0x8000000c, 0x00408093);
-  pmem_write(0x80000010, 0x00508093);
+  paddr_write(0x80000000, 0x00108093);
+  paddr_write(0x80000004, 0x00208093);
+  paddr_write(0x80000008, 0x00308093);
+  paddr_write(0x8000000c, 0x00408093);
+  paddr_write(0x80000010, 0x00508093);
 
  for(int i=0; i<5 ; i++){
     contextp->timeInc(1);
-    top->inst = pmem_read(top->pc);
+    top->inst = paddr_read(top->pc);
     top->eval();
     tfp->dump(contextp->time());
   }
