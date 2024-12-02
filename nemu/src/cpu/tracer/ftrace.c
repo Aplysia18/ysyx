7de31955abb42c7abb7935ad3c3c66b15ebc8c58
tracer-ysyx <tracer@ysyx.org>
2024-12-02 23:09:49 +0800

    > compile NEMU ysyx_24110015 鹿天瑶 Linux LAPTOP-T76LQ53S 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:09:49 up 8 days,  6:52,  1 user,  load average: 0.16, 0.12, 0.14

--- nemu/src/cpu/tracer/ftrace.c
+++ nemu/src/cpu/tracer/ftrace.c
@@ -91,7 +91,7 @@ void init_elf(const char *elf_file) {
 void ftrace_call(vaddr_t pc, vaddr_t next_pc) {
     printf(FMT_WORD ": ", pc);
     for (int i = 0; i < function_num; i++) {
-        if (next_pc == functions[i].start) {
+        if (next_pc == functions[i].start && next_pc < functions[i].start + functions[i].size) {
             for(int j = 0; j < ftrace_tab_size; j++) {
                 printf("  ");
             }
