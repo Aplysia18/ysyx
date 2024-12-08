9211deffe31028591624ce251593ffb163966606
tracer-ysyx <tracer@ysyx.org>
2024-12-08 17:23:50 +0800

    > sim RTL ysyx_24110015 鹿天瑶 Linux LAPTOP-T76LQ53S 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:23:50 up 5 days,  1:01,  1 user,  load average: 0.25, 0.15, 0.09

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,9 +1,13 @@
 #include "Vysyx_24110015_top.h"
+#include "Vysyx_24110015_top___024root.h"
 #include "verilated.h"
 #include "verilated_vcd_c.h"
 #include "svdpi.h"
 #include "Vysyx_24110015_top__Dpi.h"
 #include "monitor.hpp"
+#include "utils.hpp"
+
+Vysyx_24110015_top* top;
 
 static void single_cycle(Vysyx_24110015_top* top, VerilatedContext* contextp, VerilatedVcdC* tfp) {
   top->clk = 1;
@@ -25,6 +29,8 @@ static void reset(Vysyx_24110015_top* top, VerilatedContext* contextp, Verilated
 static bool end_flag = 0;
 
 void npc_trap(){
+  int code = top->rootp->ysyx_24110015_top__DOT__rf__DOT__rf[10];
+  printf("npc: %s at pc = 0x%08d\n", (code == 0 ? ANSI_FMT("HIT GOOD TRAP", ANSI_FG_GREEN) : ANSI_FMT("HIT BAD TRAP", ANSI_FG_RED)), top->pc);
   end_flag = 1;
 } 
 
@@ -32,7 +38,7 @@ int main(int argc, char** argv) {
 
   VerilatedContext* contextp = new VerilatedContext;
   contextp->commandArgs(argc, argv);
-  Vysyx_24110015_top* top = new Vysyx_24110015_top{contextp};
+  top = new Vysyx_24110015_top{contextp};
 
   VerilatedVcdC* tfp = new VerilatedVcdC;
   Verilated::traceEverOn(true);
