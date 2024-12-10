6be2c9d315398f75ecf0261b740b3d3c1eb72f32
tracer-ysyx <tracer@ysyx.org>
2024-12-10 15:59:47 +0800

    > sim RTL ysyx_24110015 鹿天瑶 Linux LAPTOP-T76LQ53S 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:59:47 up  5:41,  1 user,  load average: 0.14, 0.08, 0.09

--- npc/csrc/src/isa/reg.cpp
+++ npc/csrc/src/isa/reg.cpp
@@ -3,8 +3,8 @@
 const char *regs[] = {
   "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
   "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
-  "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
-  "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
+//   "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
+//   "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
 };
 
 void isa_reg_display() {
@@ -22,7 +22,7 @@ word_t isa_reg_str2val(const char *s, bool *success) {
     return 0;
   }
  
-  for(int i = 1; i <32; i++){
+  for(int i = 1; i <16; i++){
     if(strcmp(s, regs[i]) == 0){
       return gpr(i);
     }
