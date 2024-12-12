4dda8556d650610a18ede19d78a61e31b3edacf7
tracer-ysyx <tracer@ysyx.org>
2024-12-12 17:54:16 +0800

    > sim RTL ysyx_24110015 鹿天瑶 Linux LAPTOP-T76LQ53S 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:54:16 up  3:21,  1 user,  load average: 0.10, 0.09, 0.09

--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -14,7 +14,7 @@ LDFLAGS   += -T $(AM_HOME)/scripts/linker.ld \
 LDFLAGS   += --gc-sections -e _start
 CFLAGS += -DMAINARGS=\"$(mainargs)\"
 
-NPCFLAGS += -l $(shell dirname $(IMAGE).elf)/nemu-log.txt -b
+NPCFLAGS += -l $(shell dirname $(IMAGE).elf)/npc-log.txt -b
 NPCFLAGS += -e $(IMAGE).elf
 
 .PHONY: $(AM_HOME)/am/src/riscv/npc/trm.c
