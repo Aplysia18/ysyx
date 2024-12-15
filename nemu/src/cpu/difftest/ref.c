38d2607361654876583eeeec2addb6b5e93603d7
tracer-ysyx <tracer@ysyx.org>
2024-12-15 19:08:05 +0800

    > compile NEMU ysyx_24110015 鹿天瑶 Linux LAPTOP-T76LQ53S 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:08:05 up 3 days,  4:35,  1 user,  load average: 0.11, 0.08, 0.05

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -19,15 +19,29 @@
 #include <memory/paddr.h>
 
 __EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
-  assert(0);
+  if (direction == DIFFTEST_TO_REF) {
+    for (size_t i = 0; i < n; i++) {
+      paddr_write(addr+i, 1, *((uint8_t*)buf+i));
+    }
+  } else {
+    assert(0);
+  }
 }
 
 __EXPORT void difftest_regcpy(void *dut, bool direction) {
-  assert(0);
+  if (direction == DIFFTEST_TO_REF){
+    CPU_state *ref_r = (CPU_state *)dut;
+    for (size_t i = 0; i < 32; i++) {
+      ref_r->gpr[i] = cpu.gpr[i];
+    }
+    ref_r->pc = cpu.pc;
+  } else {
+    assert(0);
+  }
 }
 
 __EXPORT void difftest_exec(uint64_t n) {
-  assert(0);
+  cpu_exec(n);
 }
 
 __EXPORT void difftest_raise_intr(word_t NO) {
