4234f0c054f1c13bce28565147953003250b2784
tracer-ysyx <tracer@ysyx.org>
2024-12-15 22:02:20 +0800

    > sim RTL ysyx_24110015 鹿天瑶 Linux LAPTOP-T76LQ53S 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:02:20 up 3 days,  7:29,  1 user,  load average: 0.32, 0.60, 0.66

--- npc/csrc/src/cpu/difftest/dut.cpp
+++ npc/csrc/src/cpu/difftest/dut.cpp
@@ -140,5 +140,7 @@ void difftest_step(vaddr_t pc, vaddr_t npc) {
   checkregs(&ref_r, pc);
 }
 #else
-void init_difftest(char *ref_so_file, long img_size, int port) { }
+void init_difftest(char *ref_so_file, long img_size, int port) {
+  printf("No difftest\n");
+ }
 #endif
