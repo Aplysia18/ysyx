527b7e16d85d548ccdf2c261ee963a7233ac78e9
tracer-ysyx <tracer@ysyx.org>
2024-12-16 14:52:53 +0800

    > compile NEMU ysyx_24110015 鹿天瑶 Linux LAPTOP-T76LQ53S 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:52:53 up 4 days, 20 min,  1 user,  load average: 1.22, 1.04, 0.63

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -37,6 +37,7 @@ __EXPORT void difftest_regcpy(void *dut, bool direction) {
       cpu.gpr[i] = dut_r->gpr[i];
     }
     cpu.pc = dut_r->pc;
+    printf("pc: %08x\n", cpu.pc);
   } else {
     for (size_t i = 0; i < RISCV_GPR_NUM; i++) {
       dut_r->gpr[i] = cpu.gpr[i];
