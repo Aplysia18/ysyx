5f2a1300395ae529d98005e3a9e9dc171fc91990
tracer-ysyx <tracer@ysyx.org>
2024-12-16 15:14:07 +0800

    > sim RTL ysyx_24110015 鹿天瑶 Linux LAPTOP-T76LQ53S 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:14:07 up 4 days, 41 min,  1 user,  load average: 0.44, 1.00, 1.01

--- npc/csrc/src/monitor/monitor.cpp
+++ npc/csrc/src/monitor/monitor.cpp
@@ -94,7 +94,7 @@ void init_monitor(int argc, char *argv[]) {
   long img_size = load_img();
 
   /* Initialize differential testing. */
-  // printf("diff_so_file: %s\n", diff_so_file);
+  printf("diff_so_file: %s\n", diff_so_file);
   init_difftest(diff_so_file, img_size, difftest_port);
 
   /* Initialize the simple debugger. */
