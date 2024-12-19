309f214f4268904d6bccbcf7be332e2036eed771
tracer-ysyx <tracer@ysyx.org>
2024-12-19 23:01:32 +0800

    > sim RTL ysyx_24110015 鹿天瑶 Linux LAPTOP-T76LQ53S 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:01:32 up 7 days,  8:28,  1 user,  load average: 0.18, 0.14, 0.08

--- npc/vsrc/ifu.v
+++ npc/vsrc/ifu.v
@@ -8,11 +8,11 @@ module ysyx_24110015_IFU (
   output reg [31:0] inst
 );
 
-  always @(negedge clk or posedge rst) begin
+  always @(pc) begin
     if (rst) begin
-      inst <= 32'b0;
+      inst = 32'b0;
     end else begin
-      inst <= pmem_read(pc);
+      inst = pmem_read(pc);
     end
   end
 
