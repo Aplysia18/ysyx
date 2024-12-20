74c58da20480ba032f25a6248dcdb15743cdbcf8
tracer-ysyx <tracer@ysyx.org>
2024-12-20 19:24:05 +0800

    > sim RTL ysyx_24110015 鹿天瑶 Linux LAPTOP-T76LQ53S 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:24:05 up 8 days,  4:51,  1 user,  load average: 0.17, 0.12, 0.12

--- npc/csrc/src/cpu/cpu-exec.cpp
+++ npc/csrc/src/cpu/cpu-exec.cpp
@@ -17,11 +17,11 @@ static uint32_t npc_dnpc = 0;
 void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
 
 static void single_cycle() {
-  top->clk = 1;
+  top->clk = 0;
   top->eval();
   tfp->dump(contextp->time());
   contextp->timeInc(1);
-  top->clk = 0;
+  top->clk = 1;
   top->eval();
   tfp->dump(contextp->time());
   contextp->timeInc(1);
