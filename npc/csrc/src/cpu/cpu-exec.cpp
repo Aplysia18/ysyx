2bc28f2b781dbfb0eae5ba26e72e2b33ea4ad78e
tracer-ysyx <tracer@ysyx.org>
2024-12-21 00:47:41 +0800

    > sim RTL ysyx_24110015 鹿天瑶 Linux LAPTOP-T76LQ53S 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  00:47:41 up 8 days, 10:15,  1 user,  load average: 0.15, 0.08, 0.08

--- npc/csrc/src/cpu/cpu-exec.cpp
+++ npc/csrc/src/cpu/cpu-exec.cpp
@@ -87,7 +87,7 @@ static void execute_once(Decode *s){
   printf("npc: pc=0x%08x, inst=0x%08x, dnpc=0x%08x\n", s->pc, s->inst, s->dnpc);
 
   //update cpu state
-  cpu.pc = npc_dnpc;
+  cpu.pc = npc_pc;
   for(int i = 0; i < 16; i++) {
     cpu.gpr[i] = top->rootp->ysyx_24110015_top__DOT__rf__DOT__rf[i];
   }
