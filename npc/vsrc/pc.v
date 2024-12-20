541611b2332a9612af6a06de5878bc6a44dc5ab7
tracer-ysyx <tracer@ysyx.org>
2024-12-20 17:19:20 +0800

    > sim RTL ysyx_24110015 鹿天瑶 Linux LAPTOP-T76LQ53S 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:19:20 up 8 days,  2:46,  1 user,  load average: 3.37, 1.05, 0.49

--- npc/vsrc/pc.v
+++ npc/vsrc/pc.v
@@ -7,7 +7,7 @@ module ysyx_24110015_Pc (
   reg [31:0] pc_pre;
   ysyx_24110015_Reg #(32, 32'h80000000) i1 (.clk(clk), .rst(rst), .din(din), .dout(pc_pre), .wen(1'b1));
   always @(posedge clk) begin
-    if (rst) dout <= dout;
+    if (rst) dout <= 32'h0;
     else dout <= pc_pre;
   end
 endmodule
