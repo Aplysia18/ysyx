0d12ec2d805ef506a0740713caa98554a6cf41a3
tracer-ysyx <tracer@ysyx.org>
2024-12-23 15:51:37 +0800

    > sim RTL ysyx_24110015 鹿天瑶 Linux LAPTOP-T76LQ53S 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:51:37 up 11 days,  1:19,  1 user,  load average: 0.74, 0.79, 0.51

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -21,6 +21,7 @@
 __EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
   if (direction == DIFFTEST_TO_REF) {
     for (size_t i = 0; i < n; i++) {
+      printf("ref: paddr_write");
       paddr_write(addr+i, 1, *((uint8_t*)buf+i));
     }
   } else {
