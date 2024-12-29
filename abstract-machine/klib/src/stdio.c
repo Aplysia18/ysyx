8ee72626f2273fa86ca948222ec0ec58fbb2addd
tracer-ysyx <tracer@ysyx.org>
2024-12-29 22:05:00 +0800

    > run NEMU ysyx_24110015 鹿天瑶 Linux LAPTOP-T76LQ53S 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:05:00 up 3 days,  6:44,  1 user,  load average: 0.23, 0.20, 0.13

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -7,94 +7,93 @@
 
 // 辅助函数，用于格式化输出
 static int vsnprintf_helper(void (*output_func)(char, void*, int), void *output_arg, const char *fmt, va_list args) {
-  assert(0);
-  // int i, j = 0;
-  // bool conver = false;
-  // bool zero_flag = false;
-  // int width = 0;
-  // for (i = 0; fmt[i] != '\0'; i++) {
-  //   if (conver) {
-  //     if(fmt[i]>='0' && fmt[i]<='9') {
-  //       if(fmt[i] == '0' && width == 0) {
-  //         zero_flag = true;
-  //       } else {
-  //         width = width * 10 + fmt[i] - '0';
-  //       }
-  //     } else {
-  //       switch (fmt[i]) {
-  //         case 's': 
-  //           conver = false;
-  //           char *str = va_arg(args, char*);
-  //           while (*str) {
-  //             output_func(*str++, output_arg, j);
-  //             j++;
-  //           }
-  //           width = 0;  //TODO: width not implemented
-  //           zero_flag = false;
-  //           break;
-  //         case 'c': 
-  //           conver = false;
-  //           char ch = va_arg(args, int);
-  //           output_func(ch, output_arg, j);
-  //           j++;
-  //           width = 0;  //TODO: width not implemented
-  //           zero_flag = false;
-  //           break;
-  //         case 'd': 
-  //           conver = false;
-  //           int num = va_arg(args, int);
-  //           if (num < 0) {
-  //             output_func('-', output_arg, j);
-  //             j++;
-  //             num = -num;
-  //             width--;
-  //           } 
-  //           int len = 0;
-  //           int num2 = num;
-  //           while (num2) {
-  //             num2 /= 10;
-  //             len++;
-  //           }
-  //           if(num==0) len=1;
-  //           if(zero_flag) {
-  //             for(int k = len; k < width; k++) {
-  //               output_func('0', output_arg, j);
-  //               j++;
-  //             }
-  //           }
-  //           char num3[11];
-  //           for (int k = len - 1; k >= 0; k--) {
-  //             if(k>=11) assert(0);
-  //             num3[k] = num % 10 + '0';
-  //             num /= 10;
-  //           }
-  //           for (int k = 0; k < len; k++) {
-  //             output_func(num3[k], output_arg, j);
-  //             j++;
-  //           }
-  //           width = 0;
-  //           zero_flag = false;
-  //           break;
-  //         case '%': 
-  //           conver = false;
-  //           output_func('%', output_arg, j);
-  //           j++;
-  //           width = 0;  //TODO: width not implemented
-  //           zero_flag = false;
-  //           break;
-  //         default: {
-  //           assert(0);
-  //         }
-  //       }
-  //     }
-  //   } else if (fmt[i] == '%') {
-  //     conver = true;
-  //   } else {
-  //     output_func(fmt[i], output_arg, j);
-  //     j++;
-  //   }
-  // }
-  // return j;
+  int i, j = 0;
+  bool conver = false;
+  bool zero_flag = false;
+  int width = 0;
+  for (i = 0; fmt[i] != '\0'; i++) {
+    if (conver) {
+      if(fmt[i]>='0' && fmt[i]<='9') {
+        if(fmt[i] == '0' && width == 0) {
+          zero_flag = true;
+        } else {
+          width = width * 10 + fmt[i] - '0';
+        }
+      } else {
+        switch (fmt[i]) {
+          case 's': 
+            conver = false;
+            char *str = va_arg(args, char*);
+            while (*str) {
+              output_func(*str++, output_arg, j);
+              j++;
+            }
+            width = 0;  //TODO: width not implemented
+            zero_flag = false;
+            break;
+          case 'c': 
+            conver = false;
+            char ch = va_arg(args, int);
+            output_func(ch, output_arg, j);
+            j++;
+            width = 0;  //TODO: width not implemented
+            zero_flag = false;
+            break;
+          case 'd': 
+            conver = false;
+            int num = va_arg(args, int);
+            if (num < 0) {
+              output_func('-', output_arg, j);
+              j++;
+              num = -num;
+              width--;
+            } 
+            int len = 0;
+            int num2 = num;
+            while (num2) {
+              num2 /= 10;
+              len++;
+            }
+            if(num==0) len=1;
+            if(zero_flag) {
+              for(int k = len; k < width; k++) {
+                output_func('0', output_arg, j);
+                j++;
+              }
+            }
+            char num3[11];
+            for (int k = len - 1; k >= 0; k--) {
+              if(k>=11) assert(0);
+              num3[k] = num % 10 + '0';
+              num /= 10;
+            }
+            for (int k = 0; k < len; k++) {
+              output_func(num3[k], output_arg, j);
+              j++;
+            }
+            width = 0;
+            zero_flag = false;
+            break;
+          case '%': 
+            conver = false;
+            output_func('%', output_arg, j);
+            j++;
+            width = 0;  //TODO: width not implemented
+            zero_flag = false;
+            break;
+          default: {
+            assert(0);
+          }
+        }
+      }
+    } else if (fmt[i] == '%') {
+      conver = true;
+    } else {
+      output_func(fmt[i], output_arg, j);
+      j++;
+    }
+  }
+  return j;
 }
 
 // 输出到控制台的函数
