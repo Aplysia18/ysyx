77e381b723c7057346dba8106c6157a923ab339f
tracer-ysyx <tracer@ysyx.org>
2025-01-05 17:47:02 +0800

    > run NEMU ysyx_24110015 鹿天瑶 Linux LAPTOP-T76LQ53S 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:47:02 up 1 day,  3:03,  1 user,  load average: 0.18, 0.17, 0.11

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,12 +4,20 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
+  int i;
+  uint32_t screen_sz = inl(VGACTL_ADDR);
+  int w = screen_sz >> 16;  // TODO: get the correct width
+  int h = screen_sz & 0xffff;  // TODO: get the correct height
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (i = 0; i < w * h; i ++) fb[i] = i;
+  outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
+  uint32_t screen_sz = inl(VGACTL_ADDR);
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = 0, .height = 0,
+    .width = screen_sz >> 16, .height = screen_sz & 0xffff,
     .vmemsz = 0
   };
 }
