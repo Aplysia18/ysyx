d47e82b6fe6371b87711602737d9062552adc0bf
tracer-ysyx <tracer@ysyx.org>
2025-01-05 15:53:01 +0800

    > run NEMU ysyx_24110015 鹿天瑶 Linux LAPTOP-T76LQ53S 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:53:01 up 1 day,  1:09,  1 user,  load average: 0.31, 0.18, 0.09

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -1,6 +1,5 @@
 #include <am.h>
 #include <nemu.h>
-#include <stdio.h>
 
 void __am_timer_init() {
   outl(RTC_ADDR, 0);
@@ -8,12 +7,9 @@ void __am_timer_init() {
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  // uint32_t low = inl(RTC_ADDR);
-  // printf("uptime before: %d\n", uptime->us);
   uptime->us = inl(RTC_ADDR+4);
   uptime->us <<= 32;
   uptime->us += inl(RTC_ADDR);
-  // printf("uptime after: %d\n", uptime->us);
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
