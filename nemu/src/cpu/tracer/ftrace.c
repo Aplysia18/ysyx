7691c2f3b4408649e7988bbccbac730411ec3bed
tracer-ysyx <tracer@ysyx.org>
2025-02-17 09:25:44 +0800

    > run NEMU ysyx_24110015 鹿天瑶 Linux LAPTOP-T76LQ53S 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  09:25:44 up 22:56,  1 user,  load average: 0.18, 0.06, 0.01

--- nemu/src/cpu/tracer/ftrace.c
+++ nemu/src/cpu/tracer/ftrace.c
@@ -22,7 +22,8 @@ void ftrace_printf(const char *format, ...) {
     va_start(args, format);
     char buffer[BUFFER_SIZE];
     vsnprintf(buffer, BUFFER_SIZE, format, args);
-    log_write("%s", buffer);
+    // log_write("%s", buffer);
+    printf("%s", buffer);
     va_end(args);
 
 }
