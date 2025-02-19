3d760c2ed02ccb52ad4039568f49e29cedcd16b5
tracer-ysyx <tracer@ysyx.org>
2025-02-19 18:59:38 +0800

    > sim RTL ysyx_24110015 鹿天瑶 Linux LAPTOP-T76LQ53S 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:59:38 up  5:29,  1 user,  load average: 0.02, 0.11, 0.10

--- npc/vsrc/exu.v
+++ npc/vsrc/exu.v
@@ -169,8 +169,8 @@ module ysyx_24110015_EXU (
   end
 
   wire [31:0] din_mstatus_ecall, din_mstatus_mret;
-  assign din_mstatus_ecall = ((dout_mstatus & 32'hffffff7f) | (((dout_mstatus >> 3) & 32'b1) << 7)) & 32'hfffffff7 | 32'h00001800;
-  assign din_mstatus_mret = ((dout_mstatus & 32'hfffffff7) | (((dout_mstatus >> 7) & 32'b1) << 3)) | 32'h80 & 32'hffffe7ff;
+  assign din_mstatus_ecall = (((dout_mstatus & 32'hffffff7f) | (((dout_mstatus >> 3) & 32'b1) << 7)) & 32'hfffffff7) | 32'h00001800;
+  assign din_mstatus_mret = (((dout_mstatus & 32'hfffffff7) | (((dout_mstatus >> 7) & 32'b1) << 3)) | 32'h80) & 32'hffffe7ff;
   assign din_mstatus = (zicsr&(imm[11:0]==12'h300)) ? csr_wdata : ecall ?  din_mstatus_ecall : mret ? din_mstatus_mret : dout_mstatus;
   assign wen_mstatus = (zicsr&(imm[11:0]==12'h300)) | ecall | mret;
 
