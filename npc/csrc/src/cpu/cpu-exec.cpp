6758021040b14b62d3ddde1eb014759ae195cb78
tracer-ysyx <tracer@ysyx.org>
2025-03-05 16:31:18 +0800

    > sim RTL ysyx_24110015 鹿天瑶 Linux LAPTOP-T76LQ53S 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:31:18 up 5 days,  3:12,  1 user,  load average: 0.35, 0.21, 0.21

--- npc/csrc/src/cpu/cpu-exec.cpp
+++ npc/csrc/src/cpu/cpu-exec.cpp
@@ -92,13 +92,16 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 
 static void execute_once(Decode *s){
   s->inst = top->inst;
-  s->pc = top->pc_next;
-  s->snpc = top->pc_next + 4;
+  s->pc = top->pc;
+  s->snpc = top->pc + 4;
   // execute
-  single_cycle();
+  do{
+    single_cycle();
+  }while(top->rootp->ysyx_24110015_top__DOT__controller__DOT__state != 1);
+  // single_cycle();
   printf("single cycle, pc = 0x%08x\n", s->pc);
   
-  s->dnpc = top->pc_next;
+  s->dnpc = top->pc;
 
   //update cpu state
   cpu.pc = top->pc_next;
