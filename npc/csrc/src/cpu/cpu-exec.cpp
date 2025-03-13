b537eeb2253bdecb67d4603c1acb61601178932c
tracer-ysyx <tracer@ysyx.org>
2025-03-13 10:21:42 +0800

    > sim RTL ysyx_24110015 鹿天瑶 Linux LAPTOP-T76LQ53S 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:21:42 up 3 min,  1 user,  load average: 0.09, 0.10, 0.04

--- npc/csrc/src/cpu/cpu-exec.cpp
+++ npc/csrc/src/cpu/cpu-exec.cpp
@@ -85,7 +85,7 @@ static void execute_once(Decode *s){
     single_cycle();
     if(top->rootp->ysyx_24110015_top__DOT__controller__DOT__state==3){
       s->inst = top->inst;
-      // printf("inst = 0x%08x\n", s->inst);
+      printf("inst = 0x%08x\n", s->inst);
     }
   }while(top->rootp->ysyx_24110015_top__DOT__controller__DOT__state != 1);
   while(top->rootp->ysyx_24110015_top__DOT__controller__DOT__state == 1){
