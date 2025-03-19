375ef6e76177b390b690bc26bab96f2ac0cd049e
tracer-ysyx <tracer@ysyx.org>
2025-03-19 21:03:59 +0800

    > sim RTL ysyx_24110015 鹿天瑶 Linux LAPTOP-T76LQ53S 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:03:59 up 1 day,  1:01,  1 user,  load average: 0.75, 0.31, 0.17

--- npc/csrc/src/cpu/cpu-exec.cpp
+++ npc/csrc/src/cpu/cpu-exec.cpp
@@ -56,7 +56,7 @@ void init_cpu(int argc, char* argv[]) {
   top->trace(tfp, 99);
   tfp->open("./build/simx.fst");
 #endif
-  printf("init cpu\n");
+  // printf("init cpu\n");
   reset(5);
   //跳过第一个周期的ifu
   do{
@@ -67,7 +67,7 @@ void init_cpu(int argc, char* argv[]) {
     single_cycle();
     // printf("state = %d\n", top->rootp->ysyx_24110015_top__DOT__controller__DOT__state);
   }
-  printf("init cpu done\n");
+  // printf("init cpu done\n");
 }
 
 bool abort_flag = 0;
@@ -92,7 +92,7 @@ static void execute_once(Decode *s){
   do{
     if(top->rootp->ysyx_24110015_top__DOT__controller__DOT__state==3){
       s->inst = top->inst;
-      // printf("inst = 0x%08x\n", s->inst);
+      printf("inst = 0x%08x\n", s->inst);
     }
     single_cycle();
   }while(top->rootp->ysyx_24110015_top__DOT__controller__DOT__state != 1);
