8cb54fafec76952ea18e8292bbfa7788842dec9b
tracer-ysyx <tracer@ysyx.org>
2025-03-20 01:26:52 +0800

    > sim RTL ysyx_24110015 鹿天瑶 Linux LAPTOP-T76LQ53S 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  01:26:52 up 1 day,  5:24,  1 user,  load average: 0.22, 0.13, 0.09

--- npc/csrc/src/cpu/difftest/dut.cpp
+++ npc/csrc/src/cpu/difftest/dut.cpp
@@ -160,7 +160,6 @@ void difftest_step(vaddr_t pc, vaddr_t npc) {
   if (is_skip_ref) {
     // to skip the checking of an instruction, just copy the reg state to reference design
     ref_difftest_regcpy(&cpu, DIFFTEST_TO_REF);
-    printf("skip\n");
     is_skip_ref = false;
     return;
   }
