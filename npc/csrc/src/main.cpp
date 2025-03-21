d23bc7a0c36b57c2272b0ed2d98f2938de1876aa
tracer-ysyx <tracer@ysyx.org>
2025-03-21 19:23:53 +0800

    > sim RTL ysyx_24110015 鹿天瑶 Linux LAPTOP-T76LQ53S 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:23:53 up 2 days, 11 min,  1 user,  load average: 0.22, 0.16, 0.12

--- npc/csrc/src/main.cpp
+++ npc/csrc/src/main.cpp
@@ -3,7 +3,11 @@
 #include <cpu/cpu.hpp>
 
 extern "C" void flash_read(int32_t addr, int32_t *data) { assert(0); }
-extern "C" void mrom_read(int32_t addr, int32_t *data) { assert(0); }
+extern "C" void mrom_read(int32_t addr, int32_t *data) 
+{ 
+  *data = 0x00100073;
+  assert(0); 
+}
 
 int main(int argc, char** argv) {
   Verilated::commandArgs(argc, argv);
--- npc/vsrc/pc.v
+++ npc/vsrc/pc.v
@@ -5,5 +5,5 @@ module ysyx_24110015_Pc (
   input [31:0] din,
   output reg [31:0] pc
 );
-  ysyx_24110015_Reg #(32, 32'h80000000) i1 (.clk(clk), .rst(rst), .din(din), .dout(pc), .wen(wen));
+  ysyx_24110015_Reg #(32, 32'h20000000) i1 (.clk(clk), .rst(rst), .din(din), .dout(pc), .wen(wen));
 endmodule
