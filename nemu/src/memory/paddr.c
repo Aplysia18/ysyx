85f4c262161d22d9089edea29cde7f1513f0aa46
tracer-ysyx <tracer@ysyx.org>
2025-03-25 17:59:08 +0800

    > sim RTL ysyx_24110015 鹿天瑶 Linux LAPTOP-T76LQ53S 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:59:08 up 1 day, 13:57,  1 user,  load average: 0.14, 0.14, 0.12

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -56,6 +56,7 @@ static void sram_write(paddr_t addr, int len, word_t data) {
 uint8_t* guest_to_host(paddr_t paddr) { return pmem + paddr - CONFIG_MBASE; }
 paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
 
+#if !defined(CONFIG_TARGET_SHARE)
 static word_t pmem_read(paddr_t addr, int len) {
   word_t ret = host_read(guest_to_host(addr), len);
   return ret;
@@ -64,6 +65,7 @@ static word_t pmem_read(paddr_t addr, int len) {
 static void pmem_write(paddr_t addr, int len, word_t data) {
   host_write(guest_to_host(addr), len, data);
 }
+#endif
 
 static void out_of_bound(paddr_t addr) {
   panic("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR "] at pc = " FMT_WORD,
