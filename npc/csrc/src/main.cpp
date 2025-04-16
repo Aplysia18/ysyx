1b47345e7040a8413157aed391738fbabab9c2f1
tracer-ysyx <tracer@ysyx.org>
2025-04-16 12:54:48 +0800

    > sim RTL ysyx_24110015 鹿天瑶 Linux LAPTOP-T76LQ53S 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:54:48 up 1 day, 9 min,  1 user,  load average: 0.50, 1.77, 1.33

--- npc/csrc/src/main.cpp
+++ npc/csrc/src/main.cpp
@@ -3,11 +3,11 @@
 #include <cpu/cpu.hpp>
 
 extern "C" void flash_read(int32_t addr, int32_t *data) { 
-  if((addr>FLASH_BASE+FLASH_SIZE-4)||(addr<FLASH_BASE)||(addr%4!=0)) {
+  if((addr>FLASH_SIZE-4)||(addr%4!=0)) {
     printf("flash_read: invalid address 0x%x\n", addr);
     assert(0);
   }
-  *data = *(int32_t *)flash_guest_to_host(addr);
+  *data = *(int32_t *)flash_guest_to_host(FLASH_BASE+addr);
 }
 extern "C" void mrom_read(int32_t addr, int32_t *data) 
 { 
