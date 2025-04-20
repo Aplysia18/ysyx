5a3013442e8e6c50a8e027978f3242f173b5a58b
tracer-ysyx <tracer@ysyx.org>
2025-04-20 16:19:33 +0800

    > sim RTL ysyx_24110015 鹿天瑶 Linux LAPTOP-T76LQ53S 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:19:33 up 5 days,  3:34,  1 user,  load average: 8.61, 12.21, 10.87

--- npc/csrc/src/cpu/difftest/dut.cpp
+++ npc/csrc/src/cpu/difftest/dut.cpp
@@ -81,6 +81,7 @@ void init_difftest(char *ref_so_file, long img_size, int port) {
   ref_difftest_memcpy(MROM_BASE, mrom_guest_to_host(MROM_BASE), img_size, DIFFTEST_TO_REF);
   ref_difftest_memcpy(FLASH_BASE, flash_guest_to_host(FLASH_BASE), FLASH_SIZE, DIFFTEST_TO_REF);
   ref_difftest_regcpy(&cpu, DIFFTEST_TO_REF);
+  printf("ref_difftest_init2\n");
 }
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
