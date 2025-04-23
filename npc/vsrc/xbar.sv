c3a26eec4b120f9ba7fad5b071e9d5d78f0927c8
tracer-ysyx <tracer@ysyx.org>
2025-04-23 11:28:06 +0800

    > sim RTL ysyx_24110015 鹿天瑶 Linux LAPTOP-T76LQ53S 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:28:06 up 6 days, 16:55,  1 user,  load average: 0.64, 0.32, 0.18

--- npc/vsrc/xbar.sv
+++ npc/vsrc/xbar.sv
@@ -2,14 +2,16 @@ module ysyx_24110015_xbar (
     input clk,
     input rst,
     axi_lite_if.slave axi_master,
-    axi_lite_if.master axi_slave_clint
+    axi_lite_if.master axi_slave_clint,
+    axi_lite_if.master axi_slave_soc
 );
 
     typedef enum logic [2:0] {IDLE, ADDR_DECODE, XFER_RD, XFER_WR, RESP_RD, RESP_WR} xbar_state_t;
     xbar_state_t state, next_state;
-    logic cur_slave;
+    logic [1:0] cur_slave; //01:clint 10:soc
 
     parameter [31:0] CLINT_BASE = 32'h02000000;
+    parameter [31:0] CLINT_SIZE = 32'h0000FFFF;
 
     // axi_lite_if axi_slave;
 
@@ -32,18 +34,38 @@ module ysyx_24110015_xbar (
                 next_state = axi_master.arvalid? XFER_RD : XFER_WR;
             end
             XFER_RD: begin
-                if(cur_slave) begin
-                    if(axi_slave_clint.rvalid & axi_master.rready) begin
-                        next_state = RESP_RD;
-                    end else next_state = XFER_RD;
-                end else next_state = IDLE;
+                case(cur_slave)
+                    2'b01: begin
+                        if(axi_slave_clint.rvalid & axi_master.rready) begin
+                            next_state = RESP_RD;
+                        end else next_state = XFER_RD;
+                    end
+                    2'b10: begin
+                        if(axi_slave_soc.rvalid & axi_master.rready) begin
+                            next_state = RESP_RD;
+                        end else next_state = XFER_RD;
+                    end
+                    default: begin
+                        next_state = IDLE;
+                    end
+                endcase
             end
             XFER_WR: begin
-                if(cur_slave) begin
-                    if(axi_slave_clint.bvalid & axi_master.bready) begin
-                        next_state = RESP_WR;
-                    end else next_state = XFER_WR;
-                end else next_state = IDLE;
+                case(cur_slave)
+                    2'b01: begin
+                        if(axi_slave_clint.bvalid & axi_master.bready) begin
+                            next_state = RESP_WR;
+                        end else next_state = XFER_WR;
+                    end
+                    2'b10: begin
+                        if(axi_slave_soc.bvalid & axi_master.bready) begin
+                            next_state = RESP_WR;
+                        end else next_state = XFER_WR;
+                    end
+                    default: begin
+                        next_state = IDLE;
+                    end
+                endcase
             end
             RESP_RD, RESP_WR: begin
                 next_state = IDLE;
@@ -58,11 +80,10 @@ module ysyx_24110015_xbar (
             cur_slave <= 0;
         end
         else if(next_state == ADDR_DECODE) begin
-            case(axi_master.arvalid? axi_master.araddr : axi_master.awaddr)
-                CLINT_BASE: cur_slave <= 1; //CLINT
-                CLINT_BASE+4: cur_slave <= 1;   //CLINT
-                default: cur_slave <= 0;
-            endcase
+            if((axi_master.arvalid? axi_master.araddr : axi_master.awaddr) >= CLINT_BASE &&
+               (axi_master.arvalid? axi_master.araddr : axi_master.awaddr) < (CLINT_BASE + CLINT_SIZE)) begin
+                cur_slave <= 2'b01; //CLINT
+            end else cur_slave <= 2'b10; //SOC
         end else begin
             cur_slave <= cur_slave;
         end
@@ -77,6 +98,12 @@ module ysyx_24110015_xbar (
         axi_slave_clint.rready = 0;
         axi_slave_clint.bready = 0;
 
+        axi_slave_soc.arvalid = 0;
+        axi_slave_soc.awvalid = 0;
+        axi_slave_soc.wvalid = 0;
+        axi_slave_soc.rready = 0;
+        axi_slave_soc.bready = 0;
+
         axi_master.arready = 0;
         axi_master.awready = 0;
         axi_master.wready = 0;
@@ -90,7 +117,7 @@ module ysyx_24110015_xbar (
         case(state)
             ADDR_DECODE: begin
                 case (cur_slave)
-                    1: begin
+                    2'b01: begin
                         axi_slave_clint.arvalid = axi_master.arvalid;
                         axi_master.arready = axi_slave_clint.arready;
 
@@ -100,28 +127,48 @@ module ysyx_24110015_xbar (
                         axi_slave_clint.wvalid = axi_master.wvalid;
                         axi_master.wready = axi_slave_clint.wready;
                     end
+                    2'b10: begin
+                        axi_slave_soc.arvalid = axi_master.arvalid;
+                        axi_master.arready = axi_slave_soc.arready;
+
+                        axi_slave_soc.awvalid = axi_master.awvalid;
+                        axi_master.awready = axi_slave_soc.awready;
+
+                        axi_slave_soc.wvalid = axi_master.wvalid;
+                        axi_master.wready = axi_slave_soc.wready;
+                    end
                     default: begin
                         axi_slave_clint.arvalid = 0;
+                        axi_slave_soc.arvalid = 0;
                         axi_master.arready = 0;
 
                         axi_slave_clint.awvalid = 0;
+                        axi_slave_soc.awvalid = 0;
                         axi_master.awready = 0;
                         
                         axi_slave_clint.wvalid = 0;
+                        axi_slave_soc.wvalid = 0;
                         axi_master.wready = 0;
                     end
                 endcase
             end
             XFER_RD: begin
                 case(cur_slave)
-                    1: begin
+                    2'b01: begin
                         axi_slave_clint.rready = axi_master.rready;
                         axi_master.rvalid = axi_slave_clint.rvalid;
                         axi_master.rdata = axi_slave_clint.rdata;
                         axi_master.rresp = axi_slave_clint.rresp;
                     end
+                    2'b10: begin
+                        axi_slave_soc.rready = axi_master.rready;
+                        axi_master.rvalid = axi_slave_soc.rvalid;
+                        axi_master.rdata = axi_slave_soc.rdata;
+                        axi_master.rresp = axi_slave_soc.rresp;
+                    end
                     default: begin
                         axi_slave_clint.rready = 0;
+                        axi_slave_soc.rready = 0;
                         axi_master.rvalid = 0;
                         axi_master.rdata = 0;
                         axi_master.rresp = 0;
@@ -130,13 +177,19 @@ module ysyx_24110015_xbar (
             end
             XFER_WR: begin
                 case(cur_slave)
-                    1: begin
+                    2'b01: begin
                         axi_slave_clint.bready = axi_master.bready;
                         axi_master.bvalid = axi_slave_clint.bvalid;
                         axi_master.bresp = axi_slave_clint.bresp;
                     end
+                    2'b10: begin
+                        axi_slave_soc.bready = axi_master.bready;
+                        axi_master.bvalid = axi_slave_soc.bvalid;
+                        axi_master.bresp = axi_slave_soc.bresp;
+                    end
                     default: begin
                         axi_slave_clint.bready = 0;
+                        axi_slave_soc.bready = 0;
                         axi_master.bvalid = 0;
                         axi_master.bresp = 0;
                     end
@@ -145,10 +198,12 @@ module ysyx_24110015_xbar (
             RESP_RD: begin
                 axi_master.rvalid = 0;
                 axi_slave_clint.rready = 0;
+                axi_slave_soc.rready = 0;
             end
             RESP_WR: begin
                 axi_master.bvalid = 0;
                 axi_slave_clint.bready = 0;
+                axi_slave_soc.bready = 0;
             end
             default: begin
                 axi_slave_clint.arvalid = 0;
@@ -156,6 +211,12 @@ module ysyx_24110015_xbar (
                 axi_slave_clint.wvalid = 0;
                 axi_slave_clint.rready = 0;
                 axi_slave_clint.bready = 0;
+                
+                axi_slave_soc.arvalid = 0;
+                axi_slave_soc.awvalid = 0;
+                axi_slave_soc.wvalid = 0;
+                axi_slave_soc.rready = 0;
+                axi_slave_soc.bready = 0;
 
                 axi_master.arready = 0;
                 axi_master.awready = 0;
@@ -173,9 +234,30 @@ module ysyx_24110015_xbar (
     // addr/data broadcast
     always @(*) begin
         axi_slave_clint.araddr = axi_master.araddr;
+        axi_slave_clint.arid = axi_master.arid;
+        axi_slave_clint.arlen = axi_master.arlen;
+        axi_slave_clint.arsize = axi_master.arsize;
+        axi_slave_clint.arburst = axi_master.arburst;
         axi_slave_clint.awaddr = axi_master.awaddr;
+        axi_slave_clint.awid = axi_master.awid;
+        axi_slave_clint.awlen = axi_master.awlen;
+        axi_slave_clint.awsize = axi_master.awsize;
+        axi_slave_clint.awburst = axi_master.awburst;
         axi_slave_clint.wdata = axi_master.wdata;
         axi_slave_clint.wstrb = axi_master.wstrb;
+
+        axi_slave_soc.araddr = axi_master.araddr;
+        axi_slave_soc.arid = axi_master.arid;
+        axi_slave_soc.arlen = axi_master.arlen;
+        axi_slave_soc.arsize = axi_master.arsize;
+        axi_slave_soc.arburst = axi_master.arburst;
+        axi_slave_soc.awaddr = axi_master.awaddr;
+        axi_slave_soc.awid = axi_master.awid;
+        axi_slave_soc.awlen = axi_master.awlen;
+        axi_slave_soc.awsize = axi_master.awsize;
+        axi_slave_soc.awburst = axi_master.awburst;
+        axi_slave_soc.wdata = axi_master.wdata;
+        axi_slave_soc.wstrb = axi_master.wstrb;
     end
 
 endmodule
