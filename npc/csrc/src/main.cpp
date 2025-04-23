8244a8632ab4c9232222fca8c3b6a353bc674208
tracer-ysyx <tracer@ysyx.org>
2025-04-23 18:56:38 +0800

    > sim RTL ysyx_24110015 鹿天瑶 Linux LAPTOP-T76LQ53S 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:56:38 up 7 days, 23 min,  1 user,  load average: 0.94, 1.62, 4.46

--- npc/csrc/src/main.cpp
+++ npc/csrc/src/main.cpp
@@ -23,14 +23,18 @@ extern "C" void mrom_read(int32_t addr, int32_t *data)
 extern "C" void psram_read(int32_t addr, char *data) {
   if(addr>=PSRAM_SIZE) {
     printf("psram_read: invalid address 0x%x\n", PSRAM_BASE+addr);
+    assert(0);
   }
+  printf("psram_read: before addr = 0x%08x, data = 0x%08x", PSRAM_BASE+addr, *data);
   *data = *(char *)psram_guest_to_host(PSRAM_BASE+addr);
+  printf("psram_read: after addr = 0x%08x, data = 0x%08x", PSRAM_BASE+addr, *data);
 }
 
 extern "C" void psram_write(int32_t addr, char data) {
   // printf("psram_write: addr = 0x%08x, data = 0x%02x\n", PSRAM_BASE + addr, data);
   if(addr>=PSRAM_SIZE) {
     printf("psram_write: invalid address 0x%x\n", PSRAM_BASE+addr);
+    assert(0);
   }
   *(char *)psram_guest_to_host(PSRAM_BASE+addr) = data;
   // printf("psram_write: end\n");
