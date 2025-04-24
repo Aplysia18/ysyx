4b88ba55f74f3e9b1c7a3f84a76653e1a11b19d2
tracer-ysyx <tracer@ysyx.org>
2025-04-24 18:53:02 +0800

    > sim RTL ysyx_24110015 鹿天瑶 Linux LAPTOP-T76LQ53S 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:53:02 up 8 days, 20 min,  1 user,  load average: 0.03, 0.08, 0.17

--- abstract-machine/scripts/platform/ysyxsoc.mk
+++ abstract-machine/scripts/platform/ysyxsoc.mk
@@ -10,7 +10,7 @@ AM_SRCS := riscv/ysyxsoc/start.S \
 
 CFLAGS    += -fdata-sections -ffunction-sections
 LDFLAGS   += -T $(AM_HOME)/am/src/riscv/ysyxsoc/linker.ld \
-						 --defsym=_pmem_start=0x30000000 --defsym=_entry_offset=0x0
+						 --defsym=_pmem_start=0x0f000000 --defsym=_entry_offset=0x0
 LDFLAGS   += --gc-sections -e _start --print-map
 CFLAGS += -DMAINARGS=\"$(mainargs)\"
 
