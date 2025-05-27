cf22d9e3c73e4c7e0d26ad806e1f4631fc84e96f
tracer-ysyx <tracer@ysyx.org>
2025-05-27 16:30:25 +0800

    > sim RTL ysyx_24110015 鹿天瑶 Linux LAPTOP-T76LQ53S 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:30:25 up 7 days, 14:04,  1 user,  load average: 0.32, 0.30, 0.33

--- abstract-machine/am/src/riscv/ysyxsoc/ioe/uart.c
+++ abstract-machine/am/src/riscv/ysyxsoc/ioe/uart.c
@@ -1,9 +1,15 @@
 #include <am.h>
 #include <riscv/riscv.h>
 
+#define UART_BASE 0x10000000L
+#define UART_TX   0
+#define UART_LCR  3
+#define UART_LSR 5
+
 void __am_uart_rx(AM_UART_RX_T *rx) {
-  uint8_t ch = inb(0x10000000);
-  if (ch == 0) {
+  uint8_t ch = inb(UART_BASE);
+  uint8_t lsr = *(volatile char *)(UART_BASE + UART_LSR);
+  if (ch == 0 || ((lsr&0x1)==0)) {
     rx->data = 0xff;
   }else {
     rx->data = ch;
